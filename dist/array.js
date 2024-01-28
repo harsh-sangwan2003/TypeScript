128b667d122ec407d53bdea9348ba61cbf8f8562
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-28 13:17:48 +0530

    added

--- dist/array.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const arr = [1, 2, 3];
-const arr2 = [1, 2, "asd"];
-// tuple
-const arr3 = [1, 2];
--- dist/variables.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const a = 2;
-const str = "hello";
-const flag = true;
