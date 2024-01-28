a21e98187ab03d9d8a1778b8fed7c6892c9df23f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-28 12:31:37 +0530

    added

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,9 @@ const btn = document.getElementById('btn');
 btn.onclick;
 const img = document.querySelector('img');
 img.src;
+const form = document.getElementById('form');
+const input = document.querySelector('input');
+form.onsubmit = (e) => {
+    e.preventDefault();
+    console.log(input.value);
+};
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@
 
 // const btn = document.getElementById('btn') as HTMLElement;
 // const btn = <document.getElementById('btn')!;
-const btn = <HTMLElement> document.getElementById('btn');
+const btn = <HTMLElement>document.getElementById('btn');
 btn.onclick;
 
-const img = <HTMLImageElement> document.querySelector('img');
+const img = <HTMLImageElement>document.querySelector('img');
 img.src;
+
+const form = document.getElementById('form') as HTMLFormElement;
+const input = document.querySelector('input') as HTMLInputElement;
+
+form.onsubmit = (e) => {
+
+    e.preventDefault();
+    console.log(input.value);
+}
